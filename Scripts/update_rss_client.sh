2f093af191e72586a3f4c9ed53ab670a7f7dd758
Jerry Arciaga <jerryarciaga11@gmail.com>
2021-05-01 19:48:38 -0600

    Added some more commands

--- Scripts/update_rss_client.sh
+++ Scripts/update_rss_client.sh
@@ -5,6 +5,11 @@
 /usr/bin/rsync -avze ssh /home/jerry/.newsboat/read.txt \
     jerry@therealraspberrypi:~/.newsboat/read.txt
 
+/usr/bin/ssh jerry@therealraspberrypi "newsboat -I ~/.newsboat/read.txt"
+
+/usr/bin/rsync -avze jerry@therealraspberrypi:~/.newsboat/cache.db \
+    /home/jerry/.newsboat/cache.db
+
 /usr/bin/newsboat -x reload
 
 if [ $? -eq 0 ]; then
