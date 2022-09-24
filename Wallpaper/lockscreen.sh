1bca5f152c81b451c5d94221c34ddca220e7e112
Jerry Arciaga <jerryarciaga11@gmail.com>
2022-09-24 14:12:37 +0900

    Renamed variables, made it more clear with documentation

--- Wallpaper/lockscreen.sh
+++ Wallpaper/lockscreen.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
 LOCKSCREEN=/tmp/lockscreen.png
-GACHA=~/.wallpaper/gacha
-OVERLAY=$GACHA/$(ls $GACHA | shuf -n 1)
+LOCKSCREEN_LOGO=~/.wallpaper/logo
+OVERLAY=$LOCKSCREEN_LOGO/$(ls $LOCKSCREEN_LOGO | shuf -n 1)
 BLUR="0x9"
 
 if [ -a $LOCKSCREEN ]; then
     rm $LOCKSCREEN
 fi
 
+# Take screenshot, blur image, overlay the randomly selected logo
+# onto it then set it as the lockscreen
 scrot $LOCKSCREEN
 mogrify $LOCKSCREEN -blur $BLUR $LOCKSCREEN
 composite -gravity Center $OVERLAY $LOCKSCREEN $LOCKSCREEN
