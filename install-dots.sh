7d34464deae9e25b687c9b499f7b57196370ea08
Jerry Arciaga <jerryarciaga11@gmail.com>
2024-03-23 11:20:28 -0700

    Initial working script

--- install-dots.sh
+++ install-dots.sh
@@ -4,12 +4,27 @@ echo "Installing dotfiles..."
 
 if [[ -a $HOME/.dotfiles ]]
     then
-      echo "Linking config files for i3..."
-      echo "Linking config files for picom..."
-      echo "Linking config files for gtk..."
-      echo "Linking Wallpaper files..."
-      echo "Linking config files for alacritty..."
-      echo "Done!"
+      # i3-wm
+      echo -n "Linking config files for i3..."
+      ln -sf $HOME/.dotfiles/Configs/i3 $HOME/.config/i3
+      echo -e "\tDONE"
+      # picom
+      echo -n "Linking config files for picom..."
+      ln -sf $HOME/.dotfiles/Configs/picom $HOME/.config/picom
+      echo -e "\tDONE"
+      # gtk
+      echo -n "Linking config files for gtk..."
+      ln -sf $HOME/.dotfiles/Configs/gtk-2.0 $HOME/.config/gtk-2.0
+      ln -sf $HOME/.dotfiles/Configs/gtk-3.0 $HOME/.config/gtk-3.0
+      echo -e "\tDONE"
+      # Wallpaper
+      echo -n "Linking Wallpaper files..."
+      ln -sf $HOME/.dotfiles/Wallpaper $HOME/.wallpaper
+      echo -e "\tDONE"
+      # alacritty
+      echo -n "Linking config files for alacritty..."
+      ln -sf $HOME/.dotfiles/Wallpaper $HOME/.config/alacritty
+      echo -e "\tDONE"
     else
-      echo "$HOME./dotfiles does not exist!"
+      echo "$HOME/.dotfiles does not exist!"
 fi
